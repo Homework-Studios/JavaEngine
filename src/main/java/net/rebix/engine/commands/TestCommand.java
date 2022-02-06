a28836693459ecbd3be9c4490ddc587268216b7b
Rebix <78218707+Reebix@users.noreply.github.com>
2022-02-06 13:09:59 +0100

    lodestonefix

--- src/main/java/net/rebix/engine/commands/TestCommand.java
+++ src/main/java/net/rebix/engine/commands/TestCommand.java
@@ -1,15 +1,9 @@
 package net.rebix.engine.commands;
 
-import net.rebix.engine.Main;
-import net.rebix.engine.items.ItemBuilder;
-import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 
 public class TestCommand implements CommandExecutor {
